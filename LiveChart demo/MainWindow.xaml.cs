084e680160ffaa2a7afb9ef59db4d5b33a3ce8df
AuntYang <auntyang@foxmail.com>
2020-09-27 18:25:53 +0800

    bug多多

--- LiveChart demo/MainWindow.xaml.cs
+++ LiveChart demo/MainWindow.xaml.cs
@@ -20,77 +20,96 @@ namespace LiveChart_demo
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+     // 数据点格式
+    public class MeasureModel
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-        // 数据点格式
-        public class MeasureModel
-        {
-            public int Index { get; set; }
-            public float Value { get; set; }
-        }
+        public int Index { get; set; }
+        public float Value { get; set; }
+    }
 
-        public partial class ConstantChangesChart : UserControl
-        {
-            private int _index;
-            public ChartValues<MeasureModel> PhaseChartValues { get; set; }
-            public ChartValues<MeasureModel> ModulusChartValues { get; set; }
-            public float PhaseValue { get; set; }
-            public float ModulusValue { get; set; }
+    public partial class ConstantChangesChart : UserControl
+    {
+        private int _index;
+        public ChartValues<MeasureModel> PhaseChartValues { get; set; }
+        public ChartValues<MeasureModel> ModulusChartValues { get; set; }
+        public float PhaseValue { get; set; }
+        public float ModulusValue { get; set; }
 
-            // 当数值被更改时，触发更新
-            public int Index
+        // 当数值被更改时，触发更新
+        public int Index
+        {
+            get { return _index; }
+            set
             {
-                get { return _index; }
-                set
-                {
-                    _index = value;
-                    Read();
-                }
+                _index = value;
+                Read();
             }
+        }
 
-            public ConstantChangesChart()
+        public ConstantChangesChart()
+        {
+            
+
+            // 设置图表的XY和数值对应
+            var mapper = Mappers.Xy<MeasureModel>()
+                .X(model => model.Index)
+                .Y(model => model.Value);
+            Charting.For<MeasureModel>(mapper);
+            PhaseChartValues = new ChartValues<MeasureModel>();
+            ModulusChartValues = new ChartValues<MeasureModel>();
+
+            DataContext = this;
+        }
+
+        // 更新图表
+        private void Read()
+        {
+            PhaseChartValues.Add(new MeasureModel
             {
-                InitializeComponent();
-
-                // 设置图表的XY和数值对应
-                var mapper = Mappers.Xy<MeasureModel>()
-                    .X(model => model.Index)
-                    .Y(model => model.Value);
-                Charting.For<MeasureModel>(mapper);
-                PhaseChartValues = new ChartValues<MeasureModel>();
-                ModulusChartValues = new ChartValues<MeasureModel>();
-
-                DataContext = this;
-            }
-
-            // 更新图表
-            private void Read()
+                Index = this.Index,
+                Value = this.PhaseValue
+            });
+            ModulusChartValues.Add(new MeasureModel
             {
-                PhaseChartValues.Add(new MeasureModel
-                {
-                    Index = this.Index,
-                    Value = this.PhaseValue
-                });
-                ModulusChartValues.Add(new MeasureModel
-                {
-                    Index = this.Index,
-                    Value = this.ModulusValue
-                });
+                Index = this.Index,
+                Value = this.ModulusValue
+            });
 
-                // 限定图表最多只有十五个元素
-                if (PhaseChartValues.Count > 15)
-                {
-                    PhaseChartValues.RemoveAt(0);
-                    ModulusChartValues.RemoveAt(0);
-                }
+            // 限定图表最多只有十五个元素
+            if (PhaseChartValues.Count > 15)
+            {
+                PhaseChartValues.RemoveAt(0);
+                ModulusChartValues.RemoveAt(0);
             }
         }
     }
-}
+    public partial class MainWindow : Window
+    {
+        private int index = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Task.Factory.StartNew(RecordData);
+        }
+        private void RecordData()
+        {
+            // 持续生成随机数，模拟数据处理过程
+            while (true)
+            {
+                Thread.Sleep(500);
+                var r = new Random();
+                float phase = r.Next(1, 7);
+                float modulus = r.Next(1, 10);
+                // 更新图表数据
+                constantChangesChart.PhaseValue = phase;
+                constantChangesChart.ModulusValue = modulus;
+                constantChangesChart.Index = index++;
+            }
+        }
+
+    }
+
     
+}
 
 
